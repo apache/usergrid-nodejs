b3327a6f6feb2e22ceb1119a4a0774e77f790c2d
brandon <brandon@behemoth.ca>
2015-11-20 13:31:07 -0800

    Added first passing tests for Usergrid.GET

--- tests/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    clientId: "YXA6GXSAACS2EeOYd20aP4G6Lw",
-    clientSecret: "YXA66BeEvgNpJBwc4PAbvZZGTVS_SSw"
-}
--- tests/usergrid.js
+++ tests/usergrid.js
@@ -3,37 +3,52 @@
 var should = require('should')
 var ok = require('objectkit')
 
-var config = require('./config')
+var config = require('../package.json')
 var Usergrid = require('../usergrid')
 var UsergridClient = require('../lib/client')
 
 describe('Usergrid', function() {
-    it('should fail to initialize without a client ID and secret', function() {
+    it('should fail to initialize without an orgId and appId', function() {
         should(function() {
             Usergrid.initialize()
         }).throw()
     })
 
-    it('should initialize when using a client ID and secret', function(done) {
-        Usergrid.initialize(config.clientId, config.clientSecret)
+    it('should initialize when using an orgId and appId', function(done) {
+        Usergrid.initialize(config.usergrid.orgId, config.usergrid.appId)
+        Usergrid.orgId.should.equal(config.usergrid.orgId)
+        Usergrid.appId.should.equal(config.usergrid.appId)
         Usergrid.should.be.ok
         done()
     })
 });
 
+describe('Usergrid.GET', function() {
+    
+    this.slow(1000)
+    this.timeout(6000)
+
+    it('should make a GET call to usergrid and retrieve entities', function(done) {
+        Usergrid.GET('tests', {
+            headers: {
+                'User-Agent': 'request'
+            }
+        }, function(err, usergridResponse) {
+            usergridResponse.statusCode.should.equal(200)
+            usergridResponse.entities.should.be.an.instanceOf(Array)
+            usergridResponse.first.should.be.an.instanceOf(Object)
+            usergridResponse.last.should.be.an.instanceOf(Object)
+            usergridResponse.first.uuid.should.be.an.instanceOf(String)
+            usergridResponse.last.uuid.should.be.an.instanceOf(String)
+            done()
+        })
+    })
+})
 
 // Usergrid.init('peter', 'frog')
 // var client = new UsergridClient('bob', 'joe')
 
-// Usergrid.GET("https://api.usergrid.com/brandon.apigee/sandbox/tests", {
-//     headers: {
-//         'User-Agent': 'request'
-//     }
-// }, function(err, usergridResponse) {
-//     console.log('test1', usergridResponse.entities.length);
-//     console.log('test2', usergridResponse.first.uuid);
-//     console.log('test3', usergridResponse.last.uuid);
-// })
+
 
 // Usergrid.POST("https://api.usergrid.com/brandon.apigee/sandbox/tests", {
 //     headers: {
