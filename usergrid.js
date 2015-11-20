3453078d34267d92972bb5b478bf2cd1f4491352
brandon <brandon@behemoth.ca>
2015-11-20 11:34:27 -0800

    Another reworking of UsergridClient/Usergrid prototype inheritance

--- usergrid.js
+++ usergrid.js
@@ -1,25 +1,15 @@
 'use strict'
 
 var UsergridClient = require('./lib/client'),
-    // ok = require('objectkit'),
+    ok = require('objectkit'),
     helpers = require('./helpers'),
     util = require("util")
 
-var Usergrid = function() {
-    var self = this
-    if (self) {
-        return self
-    } else {
-        throw new Error('Usergrid shared instance has not been initialized')
-    }
-}
-
-util.inherits(Usergrid, UsergridClient);
-
-Usergrid.prototype.initialize = function(orgId, appId, callback) {
-    var self = this
-    self = new UsergridClient(orgId, appId)
+var Usergrid = { 
+  initialize: function(orgId, appId) { 
+    Object.setPrototypeOf(Usergrid, new UsergridClient(orgId, appId)) 
+  } 
 }
 
 // Exports
-module.exports = new Usergrid
+module.exports = Usergrid
