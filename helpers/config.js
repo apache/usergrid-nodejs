3ac6d422330320190f291e28ef8076afc819db83
brandon <brandon@behemoth.ca>
2016-02-17 14:35:47 -0800

    Better method for searching for config file

--- helpers/config.js
+++ helpers/config.js
@@ -18,15 +18,21 @@ if (/mocha$/i.test(process.argv[1])) {
 } else {
     try {
         file.walkSync(appRoot, function(start, dirs, names) {
-            if (_.includes(dirs, "config") && _.includes(names, "usergrid.json")) {
-                module.exports = require(appRoot + '/config/usergrid.json')
-            } else if (_.includes(dirs, "usergrid") && _.includes(names, "config.json")) {
-                module.exports = require(appRoot + '/usergrid/config.json')
-            } else if (_.includes(names, "config.json")) {
-                module.exports = require(appRoot + '/config.json')
+            if (_.includes(names, "config.json") || _.includes(names, "usergrid.json")) {
+                var name = _.first(names.filter(function(name) {
+                    return name == "config.json" || name == "usergrid.json"
+                }).sort().reverse())
+                var configPath = util.format("%s/%s", start, name)
+                module.exports = require(configPath)
+                if (module.exports.orgId === undefined || module.exports.appId === undefined) {
+                    console.log(util.format("Config file '%s' is not a valid Usergrid configuration file", configPath))
+                    module.exports = {}
+                } else {
+                    console.log(util.format("Using config file '%s'", configPath))
+                }
             }
         })
     } catch (e) {
-        
+
     }
 }
